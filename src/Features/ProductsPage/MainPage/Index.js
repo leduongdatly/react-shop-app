af1da7a865d510e739331d09ef0bf842d44d6059
Nguyen Dinh Duong <leduongdatly@gmail.com>
2022-01-27 10:24:17 +0700

    fix bug

--- src/Features/ProductsPage/MainPage/Index.js
+++ src/Features/ProductsPage/MainPage/Index.js
@@ -16,14 +16,14 @@ const Index = () => {
     return (
         <div>
             <div className="container m-6 py-5">
-                <div className="row">
+                {/* <div className="row"> */}
                     <div className="col-12 mb-5">
                         <h1 className="display-6 fw-bolder text-center">Tất cả sản phẩm</h1>
                     </div>
                     <div className="row justify-content-center">
                         {isLoading ? <Loading /> : <ShowProducts />}
                     </div>
-                </div>
+                {/* </div> */}
             </div>
         </div>
     );
