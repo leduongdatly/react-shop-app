d1c6b19d1580b0f195de84141d831daab0d645cc
Nguyen Dinh Duong <leduongdatly@gmail.com>
2022-01-27 10:16:39 +0700

    import thu vien bootstrap

--- src/Common/Header/MainPage/Index.js
+++ src/Common/Header/MainPage/Index.js
@@ -17,7 +17,7 @@ const Index = () => {
             <nav className="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm">
                 <div className="container">
                     <NavLink to="/" className="navbar-brand fw-bold fs-4">DUONG STORE</NavLink>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
